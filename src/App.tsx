ab8b8b7908075fbecc4a695d600fa4344bd49b91
Joao <joao.g.leal@gmail.com>
2020-07-31 16:12:29 -0300

    frontend gobarber

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+// import SignIn from './pages/Signin';
+// import SignUp from './pages/SignUp';
+
+// import ToastContainer from './components/ToastContainer';
+
+import AppProvider from './hooks';
+
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
